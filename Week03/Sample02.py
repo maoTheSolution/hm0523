c7b2d4a1b831ef544782b4db50e234f734561f09
Minhyuk Adam Oh <maothesolution@gmail.com>
2024-02-02 08:49:55 +0900

    updated

---
+++ Week03/Sample02.py
@@ -0,0 +1,15 @@
+# conditional loop
+# if, elif, else
+# logical thinking 
+
+
+'''
+a leap year
+can be divided by 4
+ - yes -> cannot bt divided by 100 - yes ->  can be divided by 400 -  yes -> A leap year
+                                                                   -  no  -> not a leap year
+                                   - no  ->  not a leap year
+ - no  -> not a leap year
+'''
+
+
